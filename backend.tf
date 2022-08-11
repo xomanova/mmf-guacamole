969cdf289c0642935b03c9d38473c243d7242768
Meg Rehn <mrehn87@gmail.com>
2022-08-11 16:59:39 -0400

    nlb

--- backend.tf
+++ backend.tf
@@ -3,10 +3,6 @@ data "dns_a_record_set" "tmp_socket" {
   host = "netgames.io"
 }
 
-output "tmp_socket_addrs" {
-  value = join(",", data.dns_a_record_set.tmp_socket.addrs)
-}
-
 data "aws_ami" "al2" {
   most_recent = true
 
@@ -58,15 +54,75 @@ resource "aws_security_group" "allow_socket_connections" {
 resource "aws_instance" "socket_router" {
   ami                    = data.aws_ami.al2.id
   instance_type          = "t3.micro"
-  iam_instance_profile   = "AmazonSSMRoleForInstancesQuickSetup" # Terminal access from console
+  iam_instance_profile   = "AmazonSSMRoleForInstancesQuickSetup" # Enables terminal access from console
   vpc_security_group_ids = [aws_security_group.allow_socket_connections.id]
   tags = {
     Name = "${var.project}-websocket-forwarder"
   }
   user_data = <<EOF
 #!/bin/bash
-echo "Installing requirements" > /home/ec2-user/requirments.log
-echo "Update and save iptables" > /home/ec2-user/iptables.log
-echo "Configuring listening and forward route" > /home/ec2-user/forwarder.log
+echo 1 >/proc/sys/net/ipv4/ip_forward
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination ${data.dns_a_record_set.tmp_socket.addrs}:80
+iptables -t nat -A POSTROUTING -p tcp -d ${data.dns_a_record_set.tmp_socket.addrs} --dport 80 -j MASQUERADE
+iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination ${data.dns_a_record_set.tmp_socket.addrs}:443
+iptables -t nat -A POSTROUTING -p tcp -d ${data.dns_a_record_set.tmp_socket.addrs} --dport 443 -j MASQUERADE
+iptables-save
+echo "80 and 443 port forwarding configured to ${data.dns_a_record_set.tmp_socket.addrs}" > /home/ec2-user/forwarder.log
 EOF
+}
+
+resource "aws_lb_target_group" "socket_80" {
+  name        = "socket-80"
+  target_type = "instance"
+  port        = 80
+  protocol    = "TCP"
+}
+
+resource "aws_lb_target_group" "socket_443" {
+  name        = "socket-80"
+  target_type = "instance"
+  port        = 443
+  protocol    = "TCP"
+}
+
+resource "aws_lb_target_group_attachment" "socket_tg_80_attach" {
+  target_group_arn = aws_lb_target_group.socket_80.arn
+  target_id        = aws_instance.socket_router.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "socket_tg_443_attach" {
+  target_group_arn = aws_lb_target_group.socket_443.arn
+  target_id        = aws_instance.socket_router.id
+  port             = 443
+}
+
+resource "aws_lb" "socket_nlb" {
+  name               = "socket-nlb"
+  internal           = true
+  load_balancer_type = "network"
+}
+
+resource "aws_lb_listener" "socket_listener_80" {
+  load_balancer_arn = aws_lb.socket_nlb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.socket_80.arn
+  }
+}
+
+resource "aws_lb_listener" "socket_listener_443" {
+  load_balancer_arn = aws_lb.socket_nlb.arn
+  port              = "443"
+  protocol          = "TLS"
+  certificate_arn   = data.aws_acm_certificate.acm_cert.arn
+  alpn_policy       = "HTTP2Preferred"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.socket_443.arn
+  }
 }
